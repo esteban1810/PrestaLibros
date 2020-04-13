f02055b7c3727d70d1958e1d6e4464ee0fecf1d4
Esteban1810 <estebansevillapg@gmail.com>
2020-04-13 10:10:41 -0500

    libro

---
+++ database/migrations/2020_04_13_150654_create_libros_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('genero_id');
+            $table->string('autor','50');
+            $table->string('titulo','50');
+            $table->string('editorial','30');
+            $table->string('edicion');
+            $table->Integer('anio');
+            $table->string('descripcion');
+            $table->timestamps();
+
+            $table->foreign('genero_id')->references('id')->on('generos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
